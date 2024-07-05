ca05b11be1a22abe4dafb9c3d095d3a4540deb83
AmanTwari2002 <118037887+AmanTwari2002@users.noreply.github.com>
2024-07-05 05:26:33 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+let name="Aman"
+let age=22
+let isLoggedIn=false
+let state;
+
+
+// number=>2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value / representation of empty value
+// undefined => variable declared but value not assign 
+// symbol =>unique
+
+// object
+
+console.log(typeof name)
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+
+
+console.log(typeof undefined); // undefined is type
+console.log(typeof null); // null is object
