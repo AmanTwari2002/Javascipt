3350bc1797a107731e36717ac3701c60501be095
AmanTwari2002 <118037887+AmanTwari2002@users.noreply.github.com>
2024-07-05 05:25:48 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553 // if we create const we dont change it
+let accounEmail = "aman.20jics@gmail.com"
+var accountPassword = "1234"
+accountCity = "kaimur"
+let acoountState;
+
+//accountId =2 // not allowed
+
+accounEmail = "hc@hc.com"
+accountPassword="21212121"
+ accountCity="bhabua"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope 
+*/
+
+
+console.table([accountId,accounEmail,accountPassword,accountCity,acoountState])
