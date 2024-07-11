5960e6c3cca91e641da4f6be13f00f3b4d38e7de
AmanTwari2002 <118037887+AmanTwari2002@users.noreply.github.com>
2024-07-11 05:39:46 +0000

    loop

---
+++ 01_Basics/16_loops.js
@@ -0,0 +1,79 @@
+// for loop 
+
+// for (let i = 0; i < 10; i++) {
+//     //const element = i;
+//     console.log(i);
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element==5) {
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`outer loop :${i}`);
+//     for (let j = 0; j < 10; j++) {
+//         console.log(`innner loop ${j} and inner loop ${i}`);
+        
+//     }
+    
+// }
+
+
+// let myArray=["falsh","batman","superman"]
+// for(let i=0;i<myArray.length;i++){
+//     console.log(myArray[i]);
+    
+// }
+
+
+
+// break and continnue
+
+
+// for(let i=1;i<20;i++){
+//     if(i==5){
+//         console.log("detected 5")
+//         break;
+//     }
+//     console.log(i)
+// }
+
+
+
+// for(let i=0;i<20;i++){
+//     if(i==5){
+//         console.log("detected 5")
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+
+// let i=0
+// while(i<=10){
+//     console.log(i)
+//     i=i+2
+// }
+
+// let myArray=["flash","batman","superman"]
+// let i=0
+// while(i < myArray.length){
+//     console.log(myArray[i])
+//     i=i+1
+// }
+
+
+let score =1
+ 
+do{
+    console.log(score);
+    score++
+}
+while(score<=10)
